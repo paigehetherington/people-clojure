c8273bc0d8450a7837f5ddaa41f122d1015f90be
Paige Hetherington <herbmama@gmail.com>
2016-03-23 10:46:55 -0400

    Start web stuff

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject people-clojure "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.4.0"] ; for web
+                 [compojure "1.5.0"] ; for routes (like spring)
+                 [hiccup "1.0.5"]] ;for html (=mustache)
+
+                 
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [people-clojure.core]
   :main people-clojure.core)
--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,9 +1,12 @@
 (ns people-clojure.core
   (:require [clojure.string :as str] ;creating a new namespace
-            [clojure.walk :as walk]) ;converts keys to keywords
+            [clojure.walk :as walk] ;converts keys to keywords
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j])
+  
   (:gen-class))
 
-(defn -main []
+(defn read-people []
   (let [people (slurp "people.csv") ; reads file, defines people variable
         people (str/split-lines people) ; splits into lines, keep overwriting people variable with new fxn
         people (map (fn [line] ; map applies function to a collection
@@ -12,12 +15,21 @@
         header (first people)
         people (rest people)
         people (map (fn [line]
-                      (apply hash-map (interleave header line))) ; join 2 vectors
+                      (apply hash-map (interleave header line))) ; join 2 vectors, parse into HM
                     people) ; second argument
         people (walk/keywordize-keys people) ; keys to keywords
         people (filter (fn [line]
                         (= (:country line) "Brazil"))
                        people)]
-    (spit "filtered_people.edn" (pr-str people)) ; to write file                     
+    ;(spit "filtered_people.edn" (pr-str people)) ; to write file                     
     people))
+
+(c/defroutes app
+  (c/GET "/" request
+    "Hello, world!"))
+
+(defn -main []
+  (j/run-jetty app {:port 3000})) ; ring for web
+
+
 
